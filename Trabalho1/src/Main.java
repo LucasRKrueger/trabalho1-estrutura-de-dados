9c64a1daf815ba1d4326d74fcd5ab3f8993f7260
EduardoPhilippeCosta <epcdudu@gmail.com>
2021-04-28 20:32:17 -0300

    Create Main.java

---
+++ Trabalho1/src/Main.java
@@ -0,0 +1,82 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Main {
+
+	private JFrame frame;
+	private JTextField txtArquivo;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 275);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblArquivo = new JLabel("Arquivo:");
+		lblArquivo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblArquivo.setBounds(10, 9, 53, 20);
+		frame.getContentPane().add(lblArquivo);
+		
+		txtArquivo = new JTextField();
+		txtArquivo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtArquivo.setBounds(73, 10, 247, 19);
+		frame.getContentPane().add(txtArquivo);
+		txtArquivo.setColumns(10);
+		
+		JButton btnAnalisar = new JButton("Analisar");
+		btnAnalisar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				//ANALISAR O ARQUIVO
+				
+			}
+		});
+		btnAnalisar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnAnalisar.setBounds(330, 10, 96, 21);
+		frame.getContentPane().add(btnAnalisar);
+		
+		JTextPane txtRetorno = new JTextPane();
+		txtRetorno.setEditable(false);
+		txtRetorno.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtRetorno.setBounds(10, 39, 416, 89);
+		frame.getContentPane().add(txtRetorno);
+		
+		JTextPane txtTags = new JTextPane();
+		txtTags.setEditable(false);
+		txtTags.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtTags.setBounds(10, 138, 416, 89);
+		frame.getContentPane().add(txtTags);
+	}
+}
