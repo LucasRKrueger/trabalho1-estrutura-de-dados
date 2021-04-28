043e96f14954f32fa9b11ad95234992202b2297b
LucasRKrueger <lucasrkthd@gmail.com>
2021-04-28 20:18:08 -0300

    validar singletons

--- Trabalho1/src/AvaliaTags.java
+++ Trabalho1/src/AvaliaTags.java
@@ -2,13 +2,29 @@
 public class AvaliaTags {
 	
 	PilhaLista<String> tagsPilha = new PilhaLista<>();
-	Lista<String> tagsSemFechamento = new Lista<>();
+	Lista<String> tagsSingleton = new Lista<>();
 	Lista<String> tagsErradas = new Lista<>();
 	Lista<String> tagsCorretas = new Lista<>();
 		
-	Lista<String> tagSemFechamentoDefinidas = new Lista<>();
+	Lista<String> tagsSingletonDefinidas = new Lista<>();
 	
 	public void validarEstrutura(String html) {
 		
+	}
+	
+	public void inserirTagsSemFechamento() {
+		tagsSingletonDefinidas.inserir("!DOCTYPE");
+		tagsSingletonDefinidas.inserir("html");
+		tagsSingletonDefinidas.inserir("br");
+		tagsSingletonDefinidas.inserir("col");
+		tagsSingletonDefinidas.inserir("hr");
+		tagsSingletonDefinidas.inserir("img");
+		tagsSingletonDefinidas.inserir("input");
+		tagsSingletonDefinidas.inserir("link");
+		tagsSingletonDefinidas.inserir("param");
+		tagsSingletonDefinidas.inserir("embed");
+		tagsSingletonDefinidas.inserir("source");
+		tagsSingletonDefinidas.inserir("meta");
+		tagsSingletonDefinidas.inserir("command");
 	}
 }
