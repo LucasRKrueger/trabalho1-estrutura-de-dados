191927c0a6fae0f454bed284c1e1063367458845
LucasRKrueger <lucasrkthd@gmail.com>
2021-04-28 20:11:41 -0300

    validaEstrutura class

---
+++ Trabalho1/src/AvaliaTags.java
@@ -0,0 +1,14 @@
+
+public class AvaliaTags {
+	
+	PilhaLista<String> tagsPilha = new PilhaLista<>();
+	Lista<String> tagsSemFechamento = new Lista<>();
+	Lista<String> tagsErradas = new Lista<>();
+	Lista<String> tagsCorretas = new Lista<>();
+		
+	Lista<String> tagSemFechamentoDefinidas = new Lista<>();
+	
+	public void validarEstrutura(String html) {
+		
+	}
+}
